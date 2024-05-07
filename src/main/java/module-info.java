fef886da179dd331bb53867fc685993ab4380c25
iiakremii <mohamedakram.bachta@esprit.tn>
2024-05-07 13:04:52 +0100

    first push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module Gestion.Assurancefx {
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires jdk.compiler;
+    requires twilio;
+    requires okhttp3;
+    requires com.google.gson;
+    requires org.apache.poi.ooxml;
+    exports tn.esprit.controles;
+    opens tn.esprit.controles to javafx.fxml;
+    opens tn.esprit.entity to javafx.base;
+
+}
